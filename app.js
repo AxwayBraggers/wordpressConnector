201a131aeaab6567570df3456b0045d2f3e09d1e
tpopov94 <t.popov94@gmail.com>
2016-11-28 09:42:27 +0200

    Changed tab size

--- app.js
+++ app.js
@@ -3,24 +3,24 @@
  * be used or packaged with the actual connector when published.
  */
 var Arrow = require('arrow'),
-    server = new Arrow();
+        server = new Arrow();
 
 // TODO: Define a model that you can use when you run the connector locally for testing.
 server.addModel(Arrow.Model.extend('tbray', {
-    fields: {
-        // TODO: Add fields to it.
-        title: { type: String }
-    },
-    connector: 'com.axway.wordpress.connector'
+        fields: {
+                // TODO: Add fields to it.
+                title: {type: String}
+        },
+        connector: 'com.axway.wordpress.connector'
 }));
 
 
-server.on("starting", function() {
-    server.port = 8080;
+server.on("starting", function () {
+        server.port = 8080;
 });
 
 server.on("started", function() {
-    var connector = Arrow.getConnector("com.axway.wordpress.connector");
+        var connector = Arrow.getConnector("com.axway.wordpress.connector");
 });
 
 server.start();
