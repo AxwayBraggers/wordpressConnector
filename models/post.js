97822c1b25ca5cea8dd09f8bd644f3e4aaf4f8e4
Marin Vasilev <mvasilev@axway.com>
2016-12-01 12:40:32 +0200

    Updated post model structure

--- models/post.js
+++ models/post.js
@@ -7,10 +7,15 @@ module.exports = Arrow.Model.extend('post', {
         ID: { type: Number, required: true },
         title: { type: String, required: true },
         content: { type: String, required: true },
+        excerpt: { type: String, required: true },
+        date: { type: String, required: true },
         url: { type: String, required: true },
         image_url: { type: String }, // Url to image if there is one in the post
         likes_count: { type: String },
-        comment_count: { type: String }
+        comment_count: { type: String },
+        discussion: {type: Object},
+        like_count: {type: Number},
+        featured_image: {type: String}
     },
     connector: "com.axway.wordpress.connector"
 });
